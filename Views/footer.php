187e1cba2b484e841a278a1313bc05672e5815be
Nippur <54045811+nippur101@users.noreply.github.com>
2022-11-01 06:51:31 -0300

    botones borrar pruebas

--- Views/footer.php
+++ Views/footer.php
@@ -1,4 +1,20 @@
 </body>
+<<<<<<< Updated upstream
 <?php 
 ?>
+=======
+<?php use DAO\PetsDAO;
+                                   use Models\Pets;
+
+                         $petsDAO=new PetsDAO();
+                           $user = $_SESSION["loggedUser"] ;
+                        
+                         foreach($petsDAO->getAll() as $pets) {
+                              if($pets->getOwner()==$user->getUserID()){   
+                                echo $pets->getName();
+                            }
+                        }
+
+                              ?>
+>>>>>>> Stashed changes
 </html>
--- Views/owner-profile.php
+++ Views/owner-profile.php
@@ -9,8 +9,14 @@
 <main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
+<<<<<<< Updated upstream
                <h2 class="mb-4"><?php echo "Owner: ". $user->getFirstName()."  ".$user->getLastName().":" ?> </h2>
                <form action="<?php echo FRONT_ROOT."Home/CreatePets" ?>" method="POST" >
+=======
+               <h2 class="mb-4"><?php $user = $_SESSION["loggedUser"] ; 
+                echo "Owner: ". $user->getFirstName()."  ".$user->getLastName().":" ?> </h2>
+                 <form action="<?php echo FRONT_ROOT."Pets/CreatePets" ?>" method="POST" >
+>>>>>>> Stashed changes
                <table class="table bg-light-alpha">
                     <thead>
                          <th>ID</th>
@@ -20,11 +26,21 @@
                          <th>Raza</th>
                          <th>Video</th>
                          <th>Tipo</th>
+                         <th>Borrar</th>
                     </thead>
                     <tbody>                         
+<<<<<<< Updated upstream
                          <?php 
                          $petsDAO=new PetsDAO();
 
+=======
+                         <?php use DAO\PetsDAO;
+                                   use Models\Pets;
+
+                         $petsDAO=new PetsDAO();
+                           $user = $_SESSION["loggedUser"] ;
+                        
+>>>>>>> Stashed changes
                          foreach($petsDAO->getAll() as $pets) {
                               if($pets->getOwner()==$user->getUserID()){                              ?>
                               <tr>
@@ -35,6 +51,10 @@
                                         <th><?php echo $pets->getRaze();?></th>
                                         <th> <iframe width="200" height="150" src="<?php echo $pets->getVideo();?>" ></iframe></th>
                                         <th><?php echo $pets->getPetType();?>Mediano</th>
+
+                                        
+                                        <th><input type="reset" value="borrar" name=<?php  echo $pets->getPetId();?> formaction="<?php echo FRONT_ROOT."Pets/deletePets" ?>">
+                                   </th>
                                         
                                         
                               </tr>
