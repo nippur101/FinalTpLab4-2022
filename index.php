80a48d9a195d5b7d99febe61ba77e6971ef6ecbd
Nippur <54045811+nippur101@users.noreply.github.com>
2022-11-15 12:37:52 -0300

    ComienzoReservePDO

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+	error_reporting(E_ALL);//PONER 0 SI QQUEREMOS SACAR LOS WARNINGS
 	
 	require "Config/Autoload.php";
 	require "Config/Config.php";
